a2e67971a2dc90df3d06958dcaf070b3706e62c2
lidonghai <jacle169@21cn.com>
2017-02-24 12:07:50 +0800

    uuu

--- utility.js
+++ utility.js
@@ -1,24 +1,3 @@
-/*******************************************************************************
- * Copyright (c) 2015 IBM Corp.
- *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * and Eclipse Distribution License v1.0 which accompany this distribution.
- *
- * The Eclipse Public License is available at
- *    http://www.eclipse.org/legal/epl-v10.html
- * and the Eclipse Distribution License is available at
- *   http://www.eclipse.org/org/documents/edl-v10.php.
- *
- * Contributors:
- *    James Sutton - Initial Contribution
- *******************************************************************************/
-
-/*
-Eclipse Paho MQTT-JS Utility
-This utility can be used to test the Eclipse Paho MQTT Javascript client.
-*/
-
 // Create a client instance
 client = null;
 
